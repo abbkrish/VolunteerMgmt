e88114b3b28f7cb43e55d67ff40e204eb7f8e762
abbkrish <abbkrish@gmail.com>
2017-10-23 00:23:21 -0400

    removed opbeat setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -41,6 +41,7 @@ RAVEN_MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponse
 MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
 # opbeat integration
 # See https://opbeat.com/languages/django/
+'''
 INSTALLED_APPS += ['opbeat.contrib.django', ]
 OPBEAT = {
     'ORGANIZATION_ID': env('DJANGO_OPBEAT_ORGANIZATION_ID'),
@@ -48,7 +49,7 @@ OPBEAT = {
     'SECRET_TOKEN': env('DJANGO_OPBEAT_SECRET_TOKEN')
 }
 MIDDLEWARE = ['opbeat.contrib.django.middleware.OpbeatAPMMiddleware', ] + MIDDLEWARE
-
+'''
 
 # SECURITY CONFIGURATION
 # ------------------------------------------------------------------------------

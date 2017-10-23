00226e632d4609fdbd55bfcd966e7d49d1d20bd8
abbkrish <abbkrish@gmail.com>
2017-10-23 00:55:27 -0400

    removed aws, anmail, MEDIA URL, sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -179,6 +179,7 @@ CACHES = {
 }
 
 
+'''
 # Sentry Configuration
 SENTRY_DSN = env('DJANGO_SENTRY_DSN')
 SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
@@ -234,7 +235,7 @@ RAVEN_CONFIG = {
     'CELERY_LOGLEVEL': env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO),
     'DSN': SENTRY_DSN
 }
-
+'''
 # Custom Admin URL, use {% url 'admin:index' %}
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 

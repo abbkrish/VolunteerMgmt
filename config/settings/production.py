c8d0cba302f3e3a5ffe336d70db5afccf81b1475
abbkrish <abbkrish@gmail.com>
2017-10-29 00:19:00 -0400

    Removed Sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -188,7 +188,7 @@ CACHES = {
 }
 
 
-
+'''
 # Sentry Configuration
 SENTRY_DSN = env('DJANGO_SENTRY_DSN')
 SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
@@ -244,7 +244,7 @@ RAVEN_CONFIG = {
     'CELERY_LOGLEVEL': env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO),
     'DSN': SENTRY_DSN
 }
-
+'''
 
 
 # Custom Admin URL, use {% url 'admin:index' %}
